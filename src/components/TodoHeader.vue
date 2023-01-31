621ad920176c6de7adddc55f54edaf0abaa000d1
dmwpys <dmwpys@lineus.kr>
2023-01-31 22:44:00 +0900

    ..

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,14 +1,14 @@
  <template>
-    <div>Header</div>
+    <header>
+      <h1>To-do it!</h1>
+    </header>
     
  </template>
  
- <script>
- export default {
- 
- }
- </script>
- 
- <style>
- 
+ <style scoped>
+   h1 {
+      color : #2f3b52;
+      font-weight: 900;
+      margin: 2.5rem 0 1.5rem;
+   }
  </style>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,14 +1,53 @@
- <template>
-   <div>TodoInput</div>
-    
- </template>
- 
- <script>
- export default {
- 
- }
- </script>
- 
- <style>
-   
- </style>
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo()">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      console.log(this.newTodoItem);
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0.5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
